fdd0a7506bbc5f1062a52133208883d9e4a6479a
Moktar Jama <moktarjama@gmail.com>
2017-04-20 16:05:04 -0400

    comment out spot model bug

--- app/models/spot.rb
+++ app/models/spot.rb
@@ -17,10 +17,10 @@
 class Spot < ActiveRecord::Base
   validates :lat, :lng, :owner, :price, :location, :image_url, :description, presence: true
 
-  belongs_to (
-    :owner,
-    primary_key: :id,
-    foreign_key: :owner_id,
-    class_name: "User"
-  )
+  # belongs_to (
+  #   :owner,
+  #   primary_key: :id,
+  #   foreign_key: :owner_id,
+  #   class_name: "User"
+  # )
 end
