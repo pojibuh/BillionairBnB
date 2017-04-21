c42e7a9dc258a339f56aaf3a171b47c4c54ee21f
Moktar Jama <moktarjama@gmail.com>
2017-04-21 11:51:20 -0400

    make progress on carousel

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,53 +7,53 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 # demo_user = User.create(email: 'moktar@jama.com', fname: 'Moktar', lname: 'Jama', password: 'password')
-
-new_york = Spot.create(
-  description: 'Amazing TriBeCa penthouse',
-  image_url: 'https://www.thepinnaclelist.com/wp-content/uploads/2014/10/01-48.0-Million-Sky-Lofts-Glasshouse-Penthouse-145-Hudson-Street-New-York-NY-920x700.jpg',
-  price: 800,
-  location: 'New York City',
-  owner_id: 1,
-  lat: 20.1,
-  lng: 20.1
-)
-
-los_angeles = Spot.create(
-  description: 'Great mansion in Bel-Air',
-  image_url: 'https://s-media-cache-ak0.pinimg.com/originals/35/16/65/351665403bad01e7215aca7c2c6d67af.png',
-  price: 1000,
-  location: 'Los Angeles',
-  owner_id: 1,
-  lat: 20.1,
-  lng: 20.1
-)
-
-london = Spot.create(
-  description: 'Absolutely regal palace',
-  image_url: 'https://www.royal.uk/sites/default/files/images/feature/buckingham-palace.jpg',
-  price: 1000,
-  location: 'London',
-  owner_id: 1,
-  lat: 20.1,
-  lng: 20.1
-)
-
-kanazawa = Spot.create(
-  description: 'Historic and gorgeous castle',
-  image_url: 'https://upload.wikimedia.org/wikipedia/commons/c/cd/Kanazawa-M-5935.jpg',
-  price: 800,
-  location: 'Kanazawa',
-  owner_id: 1,
-  lat: 20.1,
-  lng: 20.1
-)
-
-dubai = Spot.create(
-  description: 'man-made island for you and your friends to enjoy',
-  image_url: 'http://www.factsinside.com/wp-content/uploads/2015/05/dubai_nakhell.jpg',
-  price: 800,
-  location: 'Dubai',
-  owner_id: 1,
-  lat: 20.1,
-  lng: 20.1
-)
+#
+# new_york = Spot.create(
+#   description: 'Amazing TriBeCa penthouse',
+#   image_url: 'https://www.thepinnaclelist.com/wp-content/uploads/2014/10/01-48.0-Million-Sky-Lofts-Glasshouse-Penthouse-145-Hudson-Street-New-York-NY-920x700.jpg',
+#   price: 800,
+#   location: 'New York City',
+#   owner_id: 1,
+#   lat: 20.1,
+#   lng: 20.1
+# )
+#
+# los_angeles = Spot.create(
+#   description: 'Great mansion in Bel-Air',
+#   image_url: 'https://s-media-cache-ak0.pinimg.com/originals/35/16/65/351665403bad01e7215aca7c2c6d67af.png',
+#   price: 1000,
+#   location: 'Los Angeles',
+#   owner_id: 1,
+#   lat: 20.1,
+#   lng: 20.1
+# )
+#
+# london = Spot.create(
+#   description: 'Absolutely regal palace',
+#   image_url: 'https://www.royal.uk/sites/default/files/images/feature/buckingham-palace.jpg',
+#   price: 1000,
+#   location: 'London',
+#   owner_id: 1,
+#   lat: 20.1,
+#   lng: 20.1
+# )
+#
+# kanazawa = Spot.create(
+#   description: 'Historic and gorgeous castle',
+#   image_url: 'https://upload.wikimedia.org/wikipedia/commons/c/cd/Kanazawa-M-5935.jpg',
+#   price: 800,
+#   location: 'Kanazawa',
+#   owner_id: 1,
+#   lat: 20.1,
+#   lng: 20.1
+# )
+#
+# dubai = Spot.create(
+#   description: 'man-made island for you and your friends to enjoy',
+#   image_url: 'http://www.factsinside.com/wp-content/uploads/2015/05/dubai_nakhell.jpg',
+#   price: 800,
+#   location: 'Dubai',
+#   owner_id: 1,
+#   lat: 20.1,
+#   lng: 20.1
+# )
--- frontend/components/spots/spot_index.jsx
+++ frontend/components/spots/spot_index.jsx
@@ -1,52 +1,37 @@
 import React from 'react';
-import { Link } from 'react-router';
+import Slider from 'react-slick';
 import SpotIndexItem from './spot_index_item';
 
 class SpotIndex extends React.Component {
 
   constructor(props) {
     super(props);
-    this.slider = 0;
-    this.shiftLeft = this.shiftLeft.bind(this);
-    this.shiftRight = this.shiftRight.bind(this);
   }
 
   componentDidMount() {
     this.props.fetchSpots();
   }
 
-  shiftLeft() {
-    if (this.slider !== 0) {
-      return this.slider -= 20;
-    }
-  }
-
-  shiftRight() {
-    debugger
-    if (this.slider < 40) {
-      return this.slider += 20;
-    }
-    debugger
-  }
-
   render() {
     const spots = Object.values(this.props.spots);
-    let slider = this.slider;
     const SpotIndexItems = spots.map((spot, idx) => {
       return <SpotIndexItem spot={spot} key={idx}/>;
     });
-    const shift = {transform: `translateX(${slider})%`};
+    const settings = {
+      infinite: true,
+      speed: 500,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      arrows: true,
+      draggable: false
+    };
     return (
       <div className="outer-carousel">
-        <button className="shift-left" onClick={() => this.shiftLeft()}>
-          <img className="left-button" src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/36/Chevron-down%2C_Web_Fundamentals.svg/2000px-Chevron-down%2C_Web_Fundamentals.svg.png"/>
-        </button>
-        <div className="carousel" style={shift}>
-          { SpotIndexItems }
-        </div>
-        <button className="shift-right" onClick={() => this.shiftRight()}>
-          <img className="right-button" src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/36/Chevron-down%2C_Web_Fundamentals.svg/2000px-Chevron-down%2C_Web_Fundamentals.svg.png"/>
-        </button>
+        <Slider {...settings}>
+          <div>
+            { SpotIndexItems }
+          </div>
+        </Slider>
       </div>
     );
   }
