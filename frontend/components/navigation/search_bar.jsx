9f2bfd861076d118d8e872c2622f61e5e0bc1c2b
Moktar Jama <moktarjama@gmail.com>
2017-04-23 00:03:58 -0400

    add transition to search bar

--- frontend/components/navigation/search_bar.jsx
+++ frontend/components/navigation/search_bar.jsx
@@ -14,25 +14,38 @@ class SearchBar extends React.Component {
     return(
         <div className="search-bar">
           <form className="search-query">
-            <div className="where">
-              <i className="fa fa-search" aria-hidden="true"></i>
-              <input
-                className="location"
-                type="search"
-                placeholder="Anywhere"/>
-            </div>
-            <div className="when">
-              <i className="fa fa-calendar-o" aria-hidden="true"></i>
-              <input />
-            </div>
-            <div className="how-many">
-              <i className="fa fa-users" aria-hidden="true"></i>
-              <input />
-            </div>
+            <input
+              className="where"
+              type="text"
+              placeholder="Anywhere"/>
+            <input className="when"/>
+            <input className="how-many"/>
           </form>
         </div>
     );
   }
 }
+
+// return(
+//     <div className="search-bar">
+//       <form className="search-query">
+//         <div className="where fuckyourmom">
+//           <i className="fa fa-search" aria-hidden="true"></i>
+//           <input
+//             className="location"
+//             type="text"
+//             placeholder="Anywhere"/>
+//         </div>
+//         <div className="when">
+//           <i className="fa fa-calendar-o" aria-hidden="true"></i>
+//           <input />
+//         </div>
+//         <div className="how-many">
+//           <i className="fa fa-users" aria-hidden="true"></i>
+//           <input />
+//         </div>
+//       </form>
+//     </div>
+// );
 
 export default SearchBar;
