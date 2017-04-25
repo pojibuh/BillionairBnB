aecd60593524be13327ed29b0e59b9fa11cf1216
Moktar Jama <moktarjama@gmail.com>
2017-04-25 16:28:59 -0400

    heroku seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -58,112 +58,112 @@
 #   lng: 55.138778
 # )
 #
-# santorini = Spot.create(
-#   description: 'seashore villa with an amazing view',
-#   image_url: 'https://t-ec.bstatic.com/images/hotel/max1024x768/731/73116664.jpg',
-#   price: 700,
-#   location: 'Santorini',
-#   owner_id: 1,
-#   lat: 36.475422,
-#   lng: 25.413763
-# )
-#
-# singapore = Spot.create(
-#   description: 'modern penthouse in a thriving Singaporean district',
-#   image_url: 'http://propertyinvestment88.sg/wp-content/uploads/2015/10/Singapore-penthouse-for-sale.jpg',
-#   price: 800,
-#   location: 'Singapore',
-#   owner_id: 1,
-#   lat: 1.305224,
-#   lng: 103.913765
-# )
-#
-# san_francisco = Spot.create(
-#   description: 'excellent getaway for tech billionaires',
-#   image_url: 'https://ap.rdcpix.com/1771840340/12a1ef9c744719a43a4890b2aee25f0fl-m0xd-w480_h480_q80.jpg',
-#   price: 1000,
-#   location: 'San Francisco',
-#   owner_id: 1,
-#   lat: 37.788497,
-#   lng: -122.457513
-# )
-#
-# hong_kong = Spot.create(
-#   description: 'an amazing getaway from the bustling metropolis',
-#   image_url: 'https://www.engelvoelkers.com/wp-content/uploads/2014/11/Capture.png',
-#   price: 1200,
-#   location: 'Hong Kong',
-#   owner_id: 1,
-#   lat: 22.246116,
-#   lng: 114.185732
-# )
-#
-# paris = Spot.create(
-#   description: 'gorgeous apartment right on the Champs-Elysees',
-#   image_url: 'http://www.private-homes.com/pictures/m/0/1-paris-apartment-rent1-bedroom-champs-elysees--etoile-foch-private-homes-3.png',
-#   price: 800,
-#   location: 'Paris',
-#   owner_id: 1,
-#   lat: 48.870086,
-#   lng: 2.306588
-# )
-#
-# miami = Spot.create(
-#   description: 'wonderful place right by the beach',
-#   image_url: 'http://i.huffpost.com/gen/765953/images/o-SUNSET-ISLANDS-MIAMI-BEACH-REAL-ESTATE-HOMES-facebook.jpg',
-#   price: 1100,
-#   location: 'Miami',
-#   owner_id: 1,
-#   lat: 25.785435,
-#   lng: -80.131368
-# )
-#
-# barcelona = Spot.create(
-#   description: 'gorgeous home located near the Sagrada Familia',
-#   image_url: 'http://barcelona-home.com/blog/wp-content/upload/2014/08/9_updown_court_win_dlesham.jpg',
-#   price: 800,
-#   location: 'Barcelona',
-#   owner_id: 1,
-#   lat: 41.377032,
-#   lng: 2.188995
-# )
-#
-# milan = Spot.create(
-#   description: 'gorgeous house in the wealthiest Milanese district',
-#   image_url: 'http://www.dreamnewmarket.it/Contents/Houses/7154/1197/xwzf0v0r.jpg',
-#   price: 1000,
-#   location: 'Milan',
-#   owner_id: 1,
-#   lat: 45.361689,
-#   lng: 9.162319
-# )
-#
-# vienna = Spot.create(
-#   description: 'apartment centrally located in Innere Stadt, Vienna',
-#   image_url: 'https://aff.bstatic.com/images/hotel/840x460/137/13767864.jpg',
-#   price: 900,
-#   location: 'Vienna',
-#   owner_id: 1,
-#   lat: 48.210068,
-#   lng: 16.378979
-# )
-#
-# rio = Spot.create(
-#   description: 'wonderful beachside Leblon apartment',
-#   image_url: 'https://www.e-architect.co.uk/images/jpgs/sao_paulo/casa_6_m160310_1.jpg',
-#   price: 1300,
-#   location: 'Rio de Janeiro',
-#   owner_id: 1,
-#   lat: -22.985907,
-#   lng: -43.224665
-# )
-#
-# cape_town = Spot.create(
-#   description: 'amazing place in the suburbs of Cape Town',
-#   image_url: 'https://businesstech.co.za/news/wp-content/uploads/2015/10/R200-million-house.png',
-#   price: 1100,
-#   location: 'Cape Town',
-#   owner_id: 1,
-#   lat: -34.032869,
-#   lng: 18.474536
-# )
+santorini = Spot.create(
+  description: 'seashore villa with an amazing view',
+  image_url: 'https://t-ec.bstatic.com/images/hotel/max1024x768/731/73116664.jpg',
+  price: 700,
+  location: 'Santorini',
+  owner_id: 1,
+  lat: 36.475422,
+  lng: 25.413763
+)
+
+singapore = Spot.create(
+  description: 'modern penthouse in a thriving Singaporean district',
+  image_url: 'http://propertyinvestment88.sg/wp-content/uploads/2015/10/Singapore-penthouse-for-sale.jpg',
+  price: 800,
+  location: 'Singapore',
+  owner_id: 1,
+  lat: 1.305224,
+  lng: 103.913765
+)
+
+san_francisco = Spot.create(
+  description: 'excellent getaway for tech billionaires',
+  image_url: 'https://ap.rdcpix.com/1771840340/12a1ef9c744719a43a4890b2aee25f0fl-m0xd-w480_h480_q80.jpg',
+  price: 1000,
+  location: 'San Francisco',
+  owner_id: 1,
+  lat: 37.788497,
+  lng: -122.457513
+)
+
+hong_kong = Spot.create(
+  description: 'an amazing getaway from the bustling metropolis',
+  image_url: 'https://www.engelvoelkers.com/wp-content/uploads/2014/11/Capture.png',
+  price: 1200,
+  location: 'Hong Kong',
+  owner_id: 1,
+  lat: 22.246116,
+  lng: 114.185732
+)
+
+paris = Spot.create(
+  description: 'gorgeous apartment right on the Champs-Elysees',
+  image_url: 'http://www.private-homes.com/pictures/m/0/1-paris-apartment-rent1-bedroom-champs-elysees--etoile-foch-private-homes-3.png',
+  price: 800,
+  location: 'Paris',
+  owner_id: 1,
+  lat: 48.870086,
+  lng: 2.306588
+)
+
+miami = Spot.create(
+  description: 'wonderful place right by the beach',
+  image_url: 'http://i.huffpost.com/gen/765953/images/o-SUNSET-ISLANDS-MIAMI-BEACH-REAL-ESTATE-HOMES-facebook.jpg',
+  price: 1100,
+  location: 'Miami',
+  owner_id: 1,
+  lat: 25.785435,
+  lng: -80.131368
+)
+
+barcelona = Spot.create(
+  description: 'gorgeous home located near the Sagrada Familia',
+  image_url: 'http://barcelona-home.com/blog/wp-content/upload/2014/08/9_updown_court_win_dlesham.jpg',
+  price: 800,
+  location: 'Barcelona',
+  owner_id: 1,
+  lat: 41.377032,
+  lng: 2.188995
+)
+
+milan = Spot.create(
+  description: 'gorgeous house in the wealthiest Milanese district',
+  image_url: 'http://www.dreamnewmarket.it/Contents/Houses/7154/1197/xwzf0v0r.jpg',
+  price: 1000,
+  location: 'Milan',
+  owner_id: 1,
+  lat: 45.361689,
+  lng: 9.162319
+)
+
+vienna = Spot.create(
+  description: 'apartment centrally located in Innere Stadt, Vienna',
+  image_url: 'https://aff.bstatic.com/images/hotel/840x460/137/13767864.jpg',
+  price: 900,
+  location: 'Vienna',
+  owner_id: 1,
+  lat: 48.210068,
+  lng: 16.378979
+)
+
+rio = Spot.create(
+  description: 'wonderful beachside Leblon apartment',
+  image_url: 'https://www.e-architect.co.uk/images/jpgs/sao_paulo/casa_6_m160310_1.jpg',
+  price: 1300,
+  location: 'Rio de Janeiro',
+  owner_id: 1,
+  lat: -22.985907,
+  lng: -43.224665
+)
+
+cape_town = Spot.create(
+  description: 'amazing place in the suburbs of Cape Town',
+  image_url: 'https://businesstech.co.za/news/wp-content/uploads/2015/10/R200-million-house.png',
+  price: 1100,
+  location: 'Cape Town',
+  owner_id: 1,
+  lat: -34.032869,
+  lng: 18.474536
+)
