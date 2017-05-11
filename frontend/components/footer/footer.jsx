7d73a25ce5222e573fcc92ddd8a18052f760050c
Moktar Jama <moktarjama@gmail.com>
2017-05-11 17:31:36 -0400

    add links to footer

--- frontend/components/footer/footer.jsx
+++ frontend/components/footer/footer.jsx
@@ -8,11 +8,14 @@ class Footer extends React.Component {
         <div className="inner-footer">
           <div className="footer-left-side">
             <p>This is a Moktar Jama production</p>
-            <p>Done for the lulz/educational purposes</p>
+            <p>Done solely for educational purposes</p>
           </div>
           <div className="footer-right-side">
-            <p>And this is where I would put my links</p>
-            <p>...if I had any</p>
+            <ul className="icons">
+              <li><a href="https://github.com/pojibuh" className="icon fa fa-github circle"></a></li>
+              <li><a href="https://angel.co/moktar-jama-1" className="icon fa fa-angellist circle"></a></li>
+              <li><a href="https://www.linkedin.com/in/moktar-jama-40631094/" className="icon fa fa-linkedin-square circle"></a></li>
+            </ul>
           </div>
         </div>
       </div>
