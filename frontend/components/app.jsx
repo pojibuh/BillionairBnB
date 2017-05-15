d93100c27973852d3d9839cdae343b2980f69299
Moktar Jama <moktarjama@gmail.com>
2017-05-15 08:59:08 -0400

    edit add component

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -3,13 +3,31 @@ import NavigationBarContainer from './navigation/navigation_bar_container';
 import ModalContainer from './modal/modal_container';
 import Footer from './footer/footer';
 
-const App = ({ children }) => (
-  <div>
-    <ModalContainer />
-    <NavigationBarContainer />
-    { children }
-    <Footer />
-  </div>
-);
+// const App = ({ children }) => (
+//   <div>
+//     <ModalContainer />
+//     <NavigationBarContainer />
+//     { children }
+//     <Footer />
+//   </div>
+// );
+
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return(
+      <div>
+        <ModalContainer />
+        <NavigationBarContainer />
+        <div> { this.props.children } </div>
+        <Footer />
+      </div>
+    )
+  }
+}
 
 export default App;
