5206010bf2e64ba9eb2df184d23257472f4bd122
Moktar Jama <moktarjama@gmail.com>
2017-05-18 09:26:46 -0400

    improve booking errors

--- app/controllers/api/bookings_controller.rb
+++ app/controllers/api/bookings_controller.rb
@@ -1,54 +1,53 @@
 class Api::BookingsController < ApplicationController
 
   def create
-    if start_date == ''
+    if start_date == nil
       render(
         json: ['Your booking must have a start date.'],
         status: 422
       )
-    elsif end_date == ''
+    elsif end_date == nil
       render(
         json: ['Your booking must have an end date.'],
         status: 422
       )
+    elsif start_date != '' && end_date != ''
+      parsed_start = Booking.date_convert(start_date)
+      parsed_end = Booking.date_convert(end_date)
+      @booking = Booking.new({
+        start_date: parsed_start,
+        end_date: parsed_end,
+        spot_id: spot_id,
+        user_id: current_user.id,
+        guest_number: guests
+      })
+
+      no_overlap = Booking.no_overlap?(parsed_start, parsed_end, spot_id)
+      guest_limit = Booking.guest_limit(spot_id)
+
+      if no_overlap && guest_limit >= guests && @booking.save
+        render :show
+      elsif no_overlap == false && guest_limit < guests
+        render(
+          json: [
+            'This booking conflicts with another. Try another date.',
+            'There are too many guests for this location.'
+          ],
+          status: 422
+        )
+      elsif no_overlap == false
+        render(
+          json: ['This booking conflicts with another. Try another date.'],
+          status: 422
+        )
+      elsif guest_limit < guests
+        render(
+          json: ['There are too many guests for this location.'],
+          status: 422
+        )
+      end
     end
 
-    parsed_start = Booking.date_convert(start_date)
-    parsed_end = Booking.date_convert(end_date)
-    @booking = Booking.new({
-      start_date: parsed_start,
-      end_date: parsed_end,
-      spot_id: spot_id,
-      user_id: current_user.id,
-      guest_number: guests
-    })
-
-    no_overlap = Booking.no_overlap?(parsed_start, parsed_end, spot_id)
-    guest_limit = Booking.guest_limit(spot_id)
-
-    if no_overlap && guest_limit >= guests && @booking.save
-      render :show
-    elsif no_overlap == false && guest_limit < guests
-      render(
-        json: [
-          'This booking conflicts with another. Try another date.',
-          'There are too many guests for this location.'
-        ],
-        status: 422
-      )
-    elsif no_overlap == false
-      render(
-        json: ['This booking conflicts with another. Try another date.'],
-        status: 422
-      )
-    elsif guest_limit < guests
-      render(
-        json: ['There are too many guests for this location.'],
-        status: 422
-      )
-    else
-      render json: @booking.errors.full_messages
-    end
   end
 
   def index
--- frontend/components/booking/booking.jsx
+++ frontend/components/booking/booking.jsx
@@ -32,7 +32,7 @@ class Booking extends React.Component {
     this.props.clear();
     let startDate = this.formatMoment(this.state.startDate);
     let endDate = this.formatMoment(this.state.endDate);
-    if (this.props.currentUser && this.state.guests > 0 && startDate.length > 0 && endDate.length > 0) {
+    if (this.props.currentUser && this.state.guests > 0) {
       this.props.createBooking({
         start_date: startDate,
         end_date: endDate,
