e945f6f2e94d66689b1eecff854fe0a518db3ad2
Moktar Jama <moktarjama@gmail.com>
2017-05-24 19:49:58 -0400

    add page readjust and start on bookings redirect logic

--- frontend/components/root.jsx
+++ frontend/components/root.jsx
@@ -8,14 +8,20 @@ import BookingIndexContainer from './booking/booking_index_container';
 import App from './app';
 
 const Root = ({ store }) => {
+  const redirectIfLoggedOut = (nextState, replace) => {
+    if (!store.getState().session.currentUser) {
+      replace('/');
+    }
+  };
+
   return (
     <Provider store={ store } >
-      <Router history={ browserHistory } >
+      <Router onUpdate={() => window.scrollTo(0, 0)} history={ browserHistory } >
         <Route path='/' component={ App } >
           <IndexRoute component={ HomepageContainer } />
           <Route path='/search' component={ SearchContainer } />
           <Route path='/spots/:spotId' component={ SpotShowContainer } />
-          <Route path='/bookings' component={ BookingIndexContainer } />
+          <Route path='/bookings' component={ BookingIndexContainer } onEnter={ redirectIfLoggedOut }/>
         </Route>
       </Router>
     </Provider>
