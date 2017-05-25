e0c3bcf7ee121bdeb56459eab8796a071998afae
Moktar Jama <moktarjama@gmail.com>
2017-05-24 21:57:01 -0400

    remove logger for heroku push

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -1,12 +1,12 @@
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
-import { createLogger } from 'redux-logger';
+// import { createLogger } from 'redux-logger';
 import RootReducer from '../reducers/root_reducer';
 
 const middlewares = [thunk];
 
 if (process.env.NODE_ENV !== 'production') {
-  middlewares.push(createLogger());
+  // middlewares.push(createLogger());
 }
 
 const configureStore = (preloadedState = {}) => (
